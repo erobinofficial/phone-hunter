8865fae366102e21e61ab60f9171d903934643c8
erobinofficial <erobin8778@gmail.com>
2023-01-23 00:40:43 +0600

    commit with 34-6

--- js/app.js
+++ js/app.js
@@ -1,13 +1,29 @@
-const loadPhones = async () => {
-    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+const loadPhones = async (search, dataLimit) => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${search}`;
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data, dataLimit);
 }
 
-
-const displayPhones = phones => {
+// display searched phones
+const displayPhones = (phones, dataLimit) => {
     const phonesContainer = document.getElementById('phone-container');
+    phonesContainer.textContent = '';
+    // 10 phones only 
+    
+    const loadMoreBtn = document.getElementById('load-more');
+    if (dataLimit && phones.length > 10) {
+      phones = phones.slice(0,10);
+      loadMoreBtn.classList.remove('d-none');
+    }
+    // no result found 
+    const noFound = document.getElementById('no-found');
+    if (phones.length === 0){
+      noFound.classList.remove('d-none');
+    }
+    else{
+      noFound.classList.add('d-none');
+    }
     phones.forEach(phone => {
         console.log(phone);
         const {image, phone_name, brand} = phone;
@@ -24,5 +40,33 @@ const displayPhones = phones => {
                     `;
         phonesContainer.appendChild(phoneDiv);
     })
+    toggleBuffering(false);
 }
-loadPhones();
+
+// toggle buffering
+
+const toggleBuffering = buffering => {
+  const toggleBuffer = document.getElementById('buffer');
+  if (buffering) {
+    toggleBuffer.classList.remove('d-none');
+  }
+  else{
+    toggleBuffer.classList.add('d-none');
+  }
+}
+// process limit 
+ const processLimit = (dataLimit) => {
+  toggleBuffering(true);
+  const search = document.getElementById('search-field').value;
+  loadPhones(search, dataLimit);
+ }
+
+// button handler
+document.getElementById('button').addEventListener('click', function(){
+  processLimit(10);
+})
+
+// load more button 
+document.getElementById('load-more-btn').addEventListener('click', function(){
+  processLimit();
+});
