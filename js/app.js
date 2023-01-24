9793824b1066436357ff1b4b6a9fe7c2a6342366
erobinofficial <erobin8778@gmail.com>
2023-01-24 23:57:29 +0600

    prettier

--- js/app.js
+++ js/app.js
@@ -3,33 +3,32 @@ const loadPhones = async (search, dataLimit) => {
   const res = await fetch(url);
   const data = await res.json();
   displayPhones(data.data, dataLimit);
-}
+};
 
 // display searched phones
 const displayPhones = (phones, dataLimit) => {
   // console.log(phones);
-  const phonesContainer = document.getElementById('phone-container');
-  phonesContainer.textContent = '';
-  // 10 phones only 
+  const phonesContainer = document.getElementById("phone-container");
+  phonesContainer.textContent = "";
+  // 10 phones only
 
-  const loadMoreBtn = document.getElementById('load-more');
+  const loadMoreBtn = document.getElementById("load-more");
   if (dataLimit && phones.length > 10) {
     phones = phones.slice(0, 10);
-    loadMoreBtn.classList.remove('d-none');
+    loadMoreBtn.classList.remove("d-none");
   }
-  // no result found 
-  const noFound = document.getElementById('no-found');
+  // no result found
+  const noFound = document.getElementById("no-found");
   if (phones.length === 0) {
-    noFound.classList.remove('d-none');
+    noFound.classList.remove("d-none");
+  } else {
+    noFound.classList.add("d-none");
   }
-  else {
-    noFound.classList.add('d-none');
-  }
-  phones.forEach(phone => {
+  phones.forEach((phone) => {
     // console.log(phone);
     const { image, phone_name, brand, slug } = phone;
-    const phoneDiv = document.createElement('div');
-    phoneDiv.classList.add('col');
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
     phoneDiv.innerHTML = `
                     <div class="card">
                         <img src="${image}" class="card-img-top" alt="...">
@@ -41,38 +40,37 @@ const displayPhones = (phones, dataLimit) => {
                       </div>
                     `;
     phonesContainer.appendChild(phoneDiv);
-  })
+  });
   toggleBuffering(false);
-}
+};
 
 // toggle buffering
 
-const toggleBuffering = buffering => {
-  const toggleBuffer = document.getElementById('buffer');
+const toggleBuffering = (buffering) => {
+  const toggleBuffer = document.getElementById("buffer");
   if (buffering) {
-    toggleBuffer.classList.remove('d-none');
+    toggleBuffer.classList.remove("d-none");
+  } else {
+    toggleBuffer.classList.add("d-none");
   }
-  else {
-    toggleBuffer.classList.add('d-none');
-  }
-}
-// process limit 
+};
+// process limit
 const processLimit = (dataLimit) => {
   toggleBuffering(true);
-  const search = document.getElementById('search-field').value;
+  const search = document.getElementById("search-field").value;
   loadPhones(search, dataLimit);
-}
+};
 
 // button handler
-document.getElementById('button').addEventListener('click', function () {
+document.getElementById("button").addEventListener("click", function () {
   processLimit(10);
-})
+});
 
-// load more button 
-document.getElementById('load-more-btn').addEventListener('click', function () {
+// load more button
+document.getElementById("load-more-btn").addEventListener("click", function () {
   processLimit();
-  const loadMoreButton = document.getElementById('load-more');
-  loadMoreButton.classList.add('d-none');
+  const loadMoreButton = document.getElementById("load-more");
+  loadMoreButton.classList.add("d-none");
 });
 
 // "Enter" Click reaction
@@ -84,23 +82,22 @@ input.addEventListener("keypress", function (event) {
   }
 });
 
-// phone details fetch data 
-const loadPhoneDetails = async id => {
+// phone details fetch data
+const loadPhoneDetails = async (id) => {
   console.log(id);
   const url = `https://openapi.programming-hero.com/api/phone/${id}`;
   const res = await fetch(url);
   const data = await res.json();
   displayPhoneDetails(data.data);
+};
 
-}
-
-// display phone details 
-const displayPhoneDetails = phone => {
+// display phone details
+const displayPhoneDetails = (phone) => {
   console.log(phone);
   const { image, name, brand, slug, mainFeatures, others } = phone;
   const { chipSet, memory, displaySize, sensors } = mainFeatures;
-  const { WLAN, Bluetooth, GPS, Radio, NFC, USB } = others || 'Not Available..';
-  const modalDetails = document.getElementById('modal-details');
+  const { WLAN, Bluetooth, GPS, Radio, NFC, USB } = others || "Not Available..";
+  const modalDetails = document.getElementById("modal-details");
   modalDetails.innerHTML = `
   <div class="modal-header">
                 <h1 class="modal-title fs-5" id="staticBackdropLabel">${name}</h1>
@@ -117,13 +114,15 @@ const displayPhoneDetails = phone => {
                 Memory : ${memory}</br>
                 Display Size: ${displaySize}</br>
                 Features:</br>
-                WLAN : ${WLAN || 'Not Available'}</br>
-                Bluetooth : ${Bluetooth || 'Not Available'}</br>
-                GPS : ${GPS || 'Not Available'}</br>
-                Radio : ${Radio || 'Not Available'}</br>
-                NFC : ${NFC || 'Not Available'}</br>
-                USB : ${USB || 'Not Available'}</br>
-                Sensors : ${sensors.map(sensor => sensor).join(", ") || "Not Available"}
+                WLAN : ${WLAN || "Not Available"}</br>
+                Bluetooth : ${Bluetooth || "Not Available"}</br>
+                GPS : ${GPS || "Not Available"}</br>
+                Radio : ${Radio || "Not Available"}</br>
+                NFC : ${NFC || "Not Available"}</br>
+                USB : ${USB || "Not Available"}</br>
+                Sensors : ${
+                  sensors.map((sensor) => sensor).join(", ") || "Not Available"
+                }
 
               </div>
               <div class="modal-footer">
@@ -131,5 +130,4 @@ const displayPhoneDetails = phone => {
               </div>
   
   `;
-
-}
+};
